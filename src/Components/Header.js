eb25e6328d9bb38546997c246aa0a65c7c2ba422
RedouanBou <redouan.bouzid96@gmail.com>
2021-04-09 10:01:04 +0200

    Change in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,18 +16,18 @@ function Header() {
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/arabisch-leren-app/">Arabisch</Link>
-                <Link className="nav-link" to="/arabisch-leren-app/add-translation">Voeg Arabisch Toe</Link>
+                <Link className="nav-link" to="/">Arabisch</Link>
+                <Link className="nav-link" to="/add-translation">Voeg Arabisch Toe</Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
 
         <Switch>
-          <Route path="/arabisch-leren-app/" exact>
+          <Route path="/" exact>
             <Translations />
           </Route>
-          <Route path="/arabisch-leren-app/add-translation">
+          <Route path="/add-translation">
             <AddTranslation />
           </Route>
         </Switch>      
